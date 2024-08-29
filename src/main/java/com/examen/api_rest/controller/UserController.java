406ad555004f79ecea856b6446ab729798af69a6
LuisaNiar <luisaniar@unisabana.edu.co>
2024-08-29 13:18:38 -0500

    CrossOrigin

--- src/main/java/com/examen/api_rest/controller/UserController.java
+++ src/main/java/com/examen/api_rest/controller/UserController.java
@@ -7,8 +7,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("api/mensajes")
+@RequestMapping("/api/mensajes")
 public class UserController {
 
     @Autowired
